8749d407f6666c265bbe967aa5940da7d89f1847
Danny Padron <danny.padron09@gmail.com>
2020-04-16 20:52:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,8 +17,12 @@ def summon_captain_planet(planeteer_calls)# code an argument here
 end
 
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)# code an argument here
+  i = 0
+  if calls.any? { |i| i.length > 4}
+    return true#test is asking to return true or false
+  end
+  return false
 end
 
 def find_the_cheese# code an argument here
